3a98fb9f1bff2a14c1b9682ba466303afe6ea665
lee nghiem <88638409+lee-butn@users.noreply.github.com>
2023-01-12 17:29:07 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { SidebarRootRootRootRoot1 } from "./SidebarRootRootRootRoot1";
+
+export default function App() {
+  document.body.style.backgroundColor = "#ffffff";
+  document.body.style.margin = "0";
+  return <SidebarRootRootRootRoot1 />;
+}
---
+++ src/SidebarRootRootRootRoot1.js
@@ -0,0 +1,364 @@
+import React from "react";
+import styled from "styled-components";
+
+export const SidebarRootRootRootRoot1 = ({}) => {
+  return (
+    <SidebarRootRootRootRoot>
+      <FlexColumn>
+        <FlexRow>
+          <Icon src={`https://file.rendit.io/n/a0hmQXwZP68PAymwjwaa.svg`} />
+          <CompanyLogo
+            src={`https://file.rendit.io/n/JHEafM0ViNKW3rM8bocF.svg`}
+          />
+        </FlexRow>
+        <Text9>Completing your credit assement</Text9>
+        <FlexRow1>
+          <Icon1 src={`https://file.rendit.io/n/ZS1m8w1f8QzIzQZzfGgu.svg`} />
+          <Text1>Contact Details</Text1>
+          <Ellipse>
+            <Ellipse1>
+              <Icon2
+                src={`https://file.rendit.io/n/oayfqPV9Mu8x4IskmLXP.svg`}
+              />
+            </Ellipse1>
+          </Ellipse>
+        </FlexRow1>
+        <FlexRow1>
+          <Icon1 src={`https://file.rendit.io/n/vo2d0ghOkkn6yZdB97lb.svg`} />
+          <Text1>Business Information</Text1>
+          <Ellipse2>
+            <Ellipse3>
+              <Icon2
+                src={`https://file.rendit.io/n/v3WvOFzvewuRGOPOeC4c.svg`}
+              />
+            </Ellipse3>
+          </Ellipse2>
+        </FlexRow1>
+        <FlexRow3>
+          <Icon5 src={`https://file.rendit.io/n/gHsGdYdxMGjXOYTE4Ixr.svg`} />
+          <Text3>Directors</Text3>
+        </FlexRow3>
+        <FlexRow4>
+          <Text4>Complete</Text4>
+          <Ellipse4>
+            <Ellipse5>
+              <Icon6
+                src={`https://file.rendit.io/n/xDidDioTQjEgNPZ7sl5i.svg`}
+              />
+            </Ellipse5>
+          </Ellipse4>
+        </FlexRow4>
+        <Component1>
+          <Text5>Selected</Text5>
+          <Icon7 src={`https://file.rendit.io/n/ElIjCyJ7TtyyQXiOb8Ji.svg`} />
+        </Component1>
+        <Component>
+          <Text6>Up Next</Text6>
+          <Icon7 src={`https://file.rendit.io/n/ElIjCyJ7TtyyQXiOb8Ji.svg`} />
+        </Component>
+        <FlexRow5>
+          <Icon5 src={`https://file.rendit.io/n/mQGlr0wUHhAcIXTn8Hwj.svg`} />
+          <Text7>Credit & Identity</Text7>
+        </FlexRow5>
+        <FlexRow6>
+          <Icon5 src={`https://file.rendit.io/n/mQGlr0wUHhAcIXTn8Hwj.svg`} />
+          <Text7>Bank History</Text7>
+        </FlexRow6>
+      </FlexColumn>
+      <FlexRow7>
+        <FlexColumn1>
+          <Icon src={`https://file.rendit.io/n/IRgjlI0YA3uYup6VPT09.svg`} />
+          <Icon src={`https://file.rendit.io/n/YCU67YUZH9qTVcDgFZmD.svg`} />
+        </FlexColumn1>
+        <FlexColumn2>
+          <Text10>Privacy Policy and T&C’s</Text10>
+          <Text11>Help & Contact</Text11>
+        </FlexColumn2>
+      </FlexRow7>
+    </SidebarRootRootRootRoot>
+  );
+};
+
+const Icon = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+const FlexRow1 = styled.div`
+  width: 241px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin: 0px 0px 24px 0px;
+  padding: 0px 12px;
+`;
+const Icon1 = styled.img`
+  width: 18px;
+  height: 18px;
+  margin: 0.75px 8px 0px 0px;
+`;
+const Text1 = styled.div`
+  width: 184px;
+  height: 17px;
+  margin: 1.25px 12px 0px 0px;
+  color: #505050;
+  font-size: 14px;
+  font-weight: 500;
+  font-family: Poppins;
+  line-height: 16.80000066757202px;
+`;
+const Icon2 = styled.img`
+  width: 13px;
+  height: 13px;
+`;
+const Icon5 = styled.img`
+  width: 18px;
+  height: 18px;
+`;
+const Icon7 = styled.img`
+  width: 16px;
+  height: 16px;
+  margin: 1px 0px 0px 0px;
+`;
+const Text7 = styled.div`
+  width: 215px;
+  height: 17px;
+  margin: 0.5px 0px 0px 0px;
+  color: #505050;
+  font-size: 14px;
+  font-weight: 500;
+  font-family: Poppins;
+  line-height: 16.80000066757202px;
+`;
+const SidebarRootRootRootRoot = styled.div`
+  height: 818px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+  padding: 24px 16px;
+  border-radius: 8px;
+  background-color: #f5f5f5;
+`;
+const FlexColumn = styled.div`
+  height: 491px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+`;
+const FlexRow = styled.div`
+  width: 265px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 0px 0px 32px 0px;
+`;
+const CompanyLogo = styled.img`
+  width: 52px;
+  height: 21px;
+`;
+const Text9 = styled.div`
+  width: 265px;
+  height: 48px;
+  margin: 0px 0px 44px 0px;
+  color: #505050;
+  font-size: 20px;
+  font-weight: 600;
+  font-family: Poppins;
+  line-height: 24px;
+  letter-spacing: -0.4px;
+`;
+const Ellipse = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/Myqjz4nyRpYwE704DtDH.svg");
+`;
+const Ellipse1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  padding: 3.25px;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/ld2HW8MgLUQXKPhKy2Zf.svg");
+`;
+const Ellipse2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/nOvZHlrVmT0e35XXuwsj.svg");
+`;
+const Ellipse3 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  padding: 3.25px;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/jz56ew8MGvkH3sxSMPr8.svg");
+`;
+const FlexRow3 = styled.div`
+  gap: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin: 0px 0px 24px 0px;
+  padding: 0px 16px 0px 12px;
+`;
+const Text3 = styled.div`
+  width: 211px;
+  height: 17px;
+  margin: 0.5px 0px 0px 0px;
+  color: #505050;
+  font-size: 14px;
+  font-weight: 500;
+  font-family: Poppins;
+  line-height: 16.80000066757202px;
+`;
+const FlexRow4 = styled.div`
+  width: 183px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 0px 0px 20px 0px;
+  padding: 0px 23px 0px 59px;
+`;
+const Text4 = styled.div`
+  width: 60px;
+  height: 18px;
+  color: #475569;
+  font-size: 12px;
+  font-family: Poppins;
+  line-height: 18px;
+  white-space: nowrap;
+`;
+const Ellipse4 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  margin: 1px 0px 0px 0px;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/yhJrSsHpfMdWJZf8guLG.svg");
+`;
+const Ellipse5 = styled.div`
+  height: 10.7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  padding: 2.67px;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/WywKCnguQjgQ88UgsTPi.svg");
+`;
+const Icon6 = styled.img`
+  width: 10.7px;
+  height: 10.7px;
+`;
+const Component1 = styled.div`
+  width: 183px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 0px 0px 20px 0px;
+  padding: 11px 22px;
+  border-width: 1px;
+  border-radius: 23px;
+  border-style: solid;
+  border-color: #8075f2;
+`;
+const Text5 = styled.div`
+  width: 53px;
+  height: 18px;
+  color: #1e293b;
+  font-size: 12px;
+  font-weight: 600;
+  font-family: Poppins;
+  line-height: 18px;
+  white-space: nowrap;
+`;
+const Component = styled.div`
+  width: 183px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 0px 0px 36px 0px;
+  padding: 0px 23px 0px 59px;
+`;
+const Text6 = styled.div`
+  width: 46px;
+  height: 18px;
+  color: #1e293b;
+  font-size: 12px;
+  font-family: Poppins;
+  line-height: 18px;
+  white-space: nowrap;
+`;
+const FlexRow5 = styled.div`
+  gap: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  margin: 0px 0px 24px 0px;
+  padding: 0px 12px;
+`;
+const FlexRow6 = styled.div`
+  gap: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px 12px;
+`;
+const FlexRow7 = styled.div`
+  gap: 12px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 0px 8px;
+`;
+const FlexColumn1 = styled.div`
+  gap: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+`;
+const FlexColumn2 = styled.div`
+  gap: 18px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  padding: 3px 0px;
+`;
+const Text10 = styled.div`
+  width: 213px;
+  height: 18px;
+  color: #505050;
+  font-size: 12px;
+  font-weight: 500;
+  font-family: Poppins;
+`;
+const Text11 = styled.div`
+  width: 92px;
+  height: 18px;
+  color: #505050;
+  font-size: 12px;
+  font-weight: 500;
+  font-family: Poppins;
+  white-space: nowrap;
+`;
